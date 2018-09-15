3e236f9535691ea1e91583280ba05c75389bf831
zcserei <z@holist.io>
2018-09-15 16:33:17 +0200

    move methods into separate describe blocks

--- helpers/calculateDueDate.test.js
+++ helpers/calculateDueDate.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai')
+
 const {
   isWorkDay,
   isWorkHour,
@@ -5,35 +7,41 @@ const {
 } = require('./calculateDueDate')
 
 describe('calculateDueDate helpers', () => {
-  it('should return true for work days', () => {
-    return true
-  }
+  describe('isWorkDay', () => {
+    it('should return true for work days', () => {
+      return true
+    }
 
-  it('should return false for non-work days', () => {
-    return true
-  }
+    it('should return false for non-work days', () => {
+      return true
+    }
+  )}
 
-  it('should return true for work hours', () => {
-    return true
-  })
+  describe('isWorkHour', () => {
+    it('should return true for work hours', () => {
+      return true
+    })
 
-  it('should return false for non-work hours', () => {
-    return true
-  })
+    it('should return false for non-work hours', () => {
+      return true
+    })
+  )}
 
-  it('should return true for work hours on work days', () => {
-    return true
-  })
+  describe('isWorkTime', () => {
+    it('should return true for work hours on work days', () => {
+      return true
+    })
 
-  it('should return false for work hours on non-work days', () => {
-    return true
-  })
+    it('should return false for work hours on non-work days', () => {
+      return true
+    })
 
-  it('should return false for non-work hours on work days', () => {
-    return true
-  })
+    it('should return false for non-work hours on work days', () => {
+      return true
+    })
 
-  it('should return false for non-work hours on non-work days', () => {
-    return true
+    it('should return false for non-work hours on non-work days', () => {
+      return true
+    })
   })
 })
