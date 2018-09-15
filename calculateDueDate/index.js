3c303f341e3c8080a49c00bb83a2fa1620b4df58
zcserei <z@holist.io>
2018-09-15 18:01:52 +0200

    error handling, test fixes (test submission dates obviously cannot be future dates)

--- calculateDueDate/index.js
+++ calculateDueDate/index.js
@@ -1,5 +1,23 @@
+const {
+  isValidSubmit
+} = require('../helpers/calculateDueDate')
+
 const calculateDueDate = (submitDate, turnaroundTime) => {
-  return true 
+  if (Object.prototype.toString.call(submitDate) !== "[object Date]") {
+    throw new Error('Submission date must be a valid Date object')
+  }
+
+  if (!isValidSubmit(submitDate)) {
+    throw new Error('Invalid submission date or time')
+  }
+
+  if (typeof turnaroundTime !== 'number') {
+    throw new Error('Turnaround time must be a number')
+  }
+
+  if (!Number.isInteger(turnaroundTime)) {
+    throw new Error('Turnaround time must be an integer')
+  }
 }
 
 module.exports = { calculateDueDate }
--- calculateDueDate/index.test.js
+++ calculateDueDate/index.test.js
@@ -5,11 +5,11 @@ const { calculateDueDate } = require('./index')
 describe('calculateDueDate', () => {
   describe('Submissions', () => {
     it('should not accept submission dates that are not Date objects', () => {
-      assert.throws(calculateDueDate('Monday morning', 42), 'Submission date must be a valid Date object')
+      assert.throws(() => { calculateDueDate('Monday morning', 42) }, 'Submission date must be a valid Date object')
     })
 
     it('should not accept turnaround times that are not numbers', () => {
-      assert.throws(calculateDueDate(new Date(2018, 8, 14, 13), 'two hours'), 'Turnaround time must be a number')
+      assert.throws(() => { calculateDueDate(new Date(2018, 8, 14, 13), 'two hours') }, 'Turnaround time must be a number')
     })
 
     // while for some values -- e.g. 3.5 below -- the calculation is straightforward,
@@ -17,23 +17,23 @@ describe('calculateDueDate', () => {
     // handling values with e.g. up to two decimal places might be a good idea, but introduces further complexity
     // and most people probably don't estimate a task to take 3.19 hours anyway
     it('should not accept turnaround times that are not integers', () => {
-      assert.throws(calculateDueDate(new Date(2018, 8, 14, 13), 3.5), 'Turnaround time must be an integer')
+      assert.throws(() => { calculateDueDate(new Date(2018, 8, 14, 13), 3.5) }, 'Turnaround time must be an integer')
     })
 
     it('should not accept invalid submission days', () => {
-      assert.throws(calculateDueDate(new Date(2018, 8, 16), 2), 'Invalid submission date or time')
+      assert.throws(() => { calculateDueDate(new Date(2018, 8, 16), 2) }, 'Invalid submission date or time')
     })
 
     it('should not accept invalid submission times', () => {
-      assert.throws(calculateDueDate(new Date(2018, 8, 17, 8), 2), 'Invalid submission date or time')
+      assert.throws(() => { calculateDueDate(new Date(2018, 8, 17, 8), 2) }, 'Invalid submission date or time')
     })
 
     it('should not accept submission times from the future', () => {
-      assert.throws(calculateDueDate(new Date(2023, 8, 14), 2), 'Invalid submission date or time')
+      assert.throws(() => { calculateDueDate(new Date(2023, 8, 14), 2) }, 'Invalid submission date or time')
     })
 
     it('should accept valid submissions', () => {
-      assert.doesNotThrow(calculateDueDate(new Date(2018, 8, 14, 13), 2), Error)
+      assert.doesNotThrow(() => { calculateDueDate(new Date(2018, 8, 14, 13), 2) }, Error)
     })
   })
 
@@ -45,17 +45,17 @@ describe('calculateDueDate', () => {
 
   describe('Calculate target date within week', () => {
     it('should calculate target date for the next day', () => {
-      assert.equal(calculateDueDate(new Date(2018, 8, 17, 9), 10).getTime(), new Date(2018, 8, 18, 11).getTime())
+      assert.equal(calculateDueDate(new Date(2018, 8, 10, 9), 10).getTime(), new Date(2018, 8, 11, 11).getTime())
     })
 
     it('should calculate target date for the day following next day', () => {
-      assert.equal(calculateDueDate(new Date(2018, 8, 17, 13), 16).getTime(), new Date(2018, 8, 19, 13).getTime())
+      assert.equal(calculateDueDate(new Date(2018, 8, 10, 13), 16).getTime(), new Date(2018, 8, 11, 13).getTime())
     })
   })
 
   describe('Calculate target date skipping weekend', () => {
     it('should calculate target date for the next week', () => {
-      assert.equal(calculateDueDate(new Date(2018, 8, 21, 13), 8).getTime(), new Date(2018, 8, 24, 13).getTime())
+      assert.equal(calculateDueDate(new Date(2018, 8, 14, 13), 8).getTime(), new Date(2018, 8, 17, 13).getTime())
     })
   
     it('should calculate target date for the week following next week', () => {
@@ -63,7 +63,7 @@ describe('calculateDueDate', () => {
       // but if you have tasks comprising 42 hours of work
       // you're doing it wrong
       // sorry, not sorry ¯\_(ツ)_/¯
-      assert.equal(calculateDueDate(new Date(2018, 8, 21, 16), 42).getTime(), new Date(2018, 9, 1, 10).getTime())
+      assert.equal(calculateDueDate(new Date(2018, 8, 14, 16), 42).getTime(), new Date(2018, 8, 24, 10).getTime())
     })
   })
 })
